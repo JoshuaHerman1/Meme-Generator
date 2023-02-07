4a939003d0aacb0b810579957c98ce7236132378
JoshuaHerman1 <joshua.herman.biz@gmail.com>
2023-02-07 13:19:07 +0100

    First and Last Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('#meme-form');
+const memesContainer = document.querySelector('#memes-container');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const imgUrl = document.querySelector('#img-url').value;
+  const topText = document.querySelector('#top-text').value;
+  const bottomText = document.querySelector('#bottom-text').value;
+
+  const meme = document.createElement('div');
+  meme.style.backgroundImage = `url(${imgUrl})`;
+
+  const top = document.createElement('h2');
+  top.innerText = topText;
+
+  const bottom = document.createElement('h2');
+  bottom.innerText = bottomText;
+
+  meme.appendChild(top);
+  meme.appendChild(bottom);
+
+  memesContainer.appendChild(meme);
+
+  form.reset();
+
+  meme.addEventListener('click', () => {
+    memesContainer.removeChild(meme);
+  });
+});
